f835821be4b53a6f68cef407c168b766decf597d
RW7 <romanwronski7@gmail.com>
2025-07-07 13:44:14 +0200

    Added more tests.

--- Tests/test_page.py
+++ Tests/test_page.py
@@ -36,13 +36,79 @@ def test_buyButton(page: Page,context):
 #     page.get_by_role("button", name="Subscribe").click()
 
 #Roadmap site
-def test_roadmap(page: Page):
+def test_roadmapSite(page: Page):
     page.goto("/")
     page.get_by_role("button", name="Roadmap").click()
     page.wait_for_load_state()
-
+    assert page.url == "https://lilai.co/roadmap.html"
     assert "Q1" in page.locator(".relative.ml-6").first.text_content()
     assert "Q2" in page.locator("div:nth-child(3) > .relative").first.text_content()
     assert "Q3" in page.locator("div:nth-child(4) > .relative").first.text_content()
     assert "Q4" in page.locator("div:nth-child(5) > .relative").first.text_content()
     assert "Stretch Goals" in page.locator("div:nth-child(6) > .relative").first.text_content()
+
+#Team site
+def test_teamSite(page: Page):
+    page.goto("/")
+    page.get_by_role("button", name="Team").click()
+    page.wait_for_load_state()
+    assert page.url == "https://lilai.co/team.html"
+
+#Bridge link
+def test_bridgeLink(page: Page,context):
+    page.goto("/")
+    with context.expect_page() as new_page_info:
+        page.get_by_role("button", name="Bridge").click()
+    new_page = new_page_info.value
+    new_page.wait_for_load_state()
+    assert "debridge" in new_page.url
+
+#Explorer link
+def test_explorerLink(page: Page,context):
+    page.goto("/")
+    with context.expect_page() as new_page_info:
+        page.get_by_role("button", name="Explorer").click()
+    new_page = new_page_info.value
+    new_page.wait_for_load_state()
+    assert "arbiscan" in new_page.url
+
+#Products overview
+def test_productsOverview(page: Page):
+    page.goto("/")
+    page.get_by_role("button", name="Products").click()
+    page.get_by_role("link", name="Products Overview").click()
+    page.wait_for_load_state()
+    assert page.url == "https://lilai.co/our_products.html"
+
+#Anti-Spam
+def test_antiSpam(page: Page):
+    page.goto("/")
+    page.get_by_role("button", name="Products").click()
+    page.get_by_role("link", name="Anti-Spam", exact=True).click()
+    page.wait_for_load_state()
+    assert page.url == "https://lilai.co/lilai_as.php"
+
+#Community Manager
+def test_communityManager(page: Page):
+    page.goto("/")
+    page.get_by_role("button", name="Products").click()
+    page.locator("#navbarNav").get_by_role("link", name="Community Manager").click()
+    page.wait_for_load_state()
+    assert page.url == "https://lilai.co/lilai_cm.php"
+
+#Data Oracle
+def test_dataOracle(page: Page):
+    page.goto("/")
+    page.get_by_role("button", name="Products").click()
+    page.get_by_role("link", name="LilAI Data Oracle").click()
+    page.wait_for_load_state()
+    assert page.url == "https://lilai.co/lilai_data_oracle.php"
+
+#Bluebird
+def test_bluebird(page: Page):
+    page.goto("/")
+    page.get_by_role("button", name="Products").click()
+    page.get_by_role("link", name="lilAI Bluebird").click()
+    page.wait_for_load_state()
+    assert page.url == "https://lilai.co/lilAI_bluebird.php"
+
